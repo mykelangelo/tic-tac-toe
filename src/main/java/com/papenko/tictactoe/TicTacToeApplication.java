b092b126bec63b13025284ed83603d60addd7435
reactive_cat <mykola.papenko@gmail.com>
2020-09-07 21:52:37 +0300

    [0] fix telegram exception: Error removing old webhook

--- src/main/java/com/papenko/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/papenko/tictactoe/TicTacToeApplication.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @SpringBootApplication
@@ -13,14 +11,6 @@ public class TicTacToeApplication {
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
-
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-
-        try {
-            telegramBotsApi.registerBot(new SandoxBot());
-        } catch (TelegramApiException e) {
-            log.error("Something went wrong during registering bot", e);
-        }
 
         SpringApplication.run(TicTacToeApplication.class, args);
     }
